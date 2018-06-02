1ef888811e1119112f89037c42810b9b4a6af477
ChrisYounger <chris.younger@jds.net.au>
2018-06-02 17:07:25 +1000

    now using vscode editor, add support for new files, folders, deleting, renaming and other security fixes

--- appserver/static/config.js
+++ appserver/static/config.js
@@ -1,3 +1,7 @@
+
+//require.config({ paths: { "app": "../app" } });
+require.config({ paths: { 'vs': '../app/config-editor/node_modules/monaco-editor/min/vs' }});
+
 require([
 	"splunkjs/mvc",
 	"jquery",
@@ -6,7 +10,8 @@ require([
 	"splunkjs/mvc/simplexml",
 	"splunkjs/mvc/layoutview",
 	"splunkjs/mvc/simplexml/dashboardview",
-    "splunkjs/mvc/searchmanager"
+    "splunkjs/mvc/searchmanager",
+    "vs/editor/editor.main"
 ], function(
 	mvc,
 	$,
@@ -15,24 +20,86 @@ require([
 	DashboardController,
 	LayoutView,
 	Dashboard,
-    SearchManager
+    SearchManager,
+    wat
 ) {
     var service = mvc.createService({ owner: "nobody" });
-    var editor = ace.edit($(".ce_contents")[0]);
-
-	editor.$blockScrolling = Infinity;	
-	editor.setTheme("ace/theme/chrome");
-	editor.setHighlightActiveLine(false);
-	editor.setShowPrintMargin(false);
-	editor.getSession().setMode("ace/mode/ini");
-	editor.setOptions({maxLines: 1000, minLines: 10});
+    console.log(monaco);
+    var editors = [];  
+	var inFolder = ".";
     var $dirlist = $(".ce_file_list");
-    $(".ce_file_list").on("click", "li", function(){
-        console.log($(this).attr('f'));
+    var $container = $(".ce_contents");
+    var $tabs = $(".ce_tabs");
+    
+	
+    $dirlist.on("click", ".ce_add_file", function(e){
+        e.stopPropagation();
+		var fname = prompt("New file name:", "");
+		if (fname) {
+			create($(this).parent().attr('f'), fname, "newfile");
+		}
+		
+    }).on("click", ".ce_add_folder", function(e){
+        e.stopPropagation();
+		var fname = prompt("New folder name:", "");
+		if (fname) {
+			create($(this).parent().attr('f'), fname, "newfolder");
+		}
+		
+    }).on("click", ".ce_rename_icon", function(e){
+        e.stopPropagation();
+		var newname = prompt("New name for [" + dodgy_basename($(this).parent().attr('f')) + "]:", "");
+		if (newname) {
+			create($(this).parent().attr('f'), newname, "rename");
+		}
+		
+    }).on("click", ".ce_delete_icon", function(e){
+        e.stopPropagation();
+		var newname = prompt("Are you sure you want to delete [" + $(this).parent().attr('f') + "]? To confirm type 'yes':", "");
+		if (newname === 'yes') {
+			create($(this).parent().attr('f'), undefined, "delete");
+		}
+		
+    }).on("click", ".ce_leftnav", function(){
         read($(this).attr('f'));
+		
+    }).on("mouseenter", ".ce_leftnav_editable", function(){
+        $(this).append("<i title='Rename' class='ce_rename_icon icon-pencil ce_right_icon ce_right_two'></i><i title='Delete' class='ce_delete_icon icon-trash ce_right_icon'></i>");
+        
+    }).on("mouseleave", ".ce_leftnav_editable", function(){
+        $(this).find('.ce_right_icon').remove();
     });
     
- 	$(window).bind('keydown', function(event) {
+    $tabs.on("click", ".ce_close_tab", function(e){
+        var idx = $(this).parent().index();
+        e.stopPropagation();
+        editors[idx].editor.dispose();
+        editors[idx].tab.remove();
+        editors[idx].container.remove();
+        editors.splice(idx, 1);
+        if ($tabs.children(".ce_active").length === 0) {
+            var last_used_idx, 
+                newest;
+            for (var i = 0; i < editors.length; i++) {
+                if (! newest || newest < editors[i].last_opened) {
+                    newest = editors[i].last_opened;
+                    last_used_idx = i;
+                }
+            }
+            activateTab(last_used_idx);
+        }
+        
+    }).on("click", ".ce_tab", function(){
+        activateTab($(this).index());
+        
+    }).on("mouseenter", ".ce_tab", function(){
+        $(this).append("<i class='ce_close_tab icon-close  ce_right_icon'></i>");
+        
+    }).on("mouseleave", ".ce_tab", function(){
+        $(this).find('.ce_close_tab').remove();
+    });
+    
+ 	/*$(window).bind('keydown', function(event) {
         if (event.ctrlKey || event.metaKey) {
             switch (String.fromCharCode(event.which).toLowerCase()) {
             case 's':
@@ -41,36 +108,112 @@ require([
                 break;
             }
         }
-    });
+    });*/
 
-    read(".");
+    read();
     
-    function read(f) {
-        service.get('/services/readfile', {file: f}, function(err, r) {
+    function activateTab(idx){
+        console.log("activating tab ", editors[idx].file);
+        hideAllTabs();
+        $container.children().eq(idx).removeClass('ce_hidden');
+        $tabs.children().eq(idx).addClass('ce_active');
+        editors[idx].last_opened = Date.now();        
+    }
+    function hideAllTabs() {
+        $container.children().addClass("ce_hidden");
+        $tabs.children().removeClass("ce_active");
+    }
+
+	function create(path, filename, type) {
+		service.get('/services/readfile', {path: path, newname: filename, action: type}, function(err, r) {
             if (err) {
                 console.log('response: ', err);
                 console.log(err.data.messages["0"].text);
             } else {
                 console.log('response: ', r);
-                if (typeof r.data.result === "string") {
-                    editor.setValue(r.data.result, 1);
-                } else {
+				read();
+				if (r.data.info === "success") {
+					alert("success");
+				} else if (r.data.info === "error") {
+					alert("ERROR: " + r.data.result)
+				}
+				// TODO if "path" is open in an editor, it needs to be closed without warning
+			}				
+		});		
+	}
+    function read(f) {
+		if (! f) {
+			f = inFolder;
+		}
+        // check if file is already open
+        for (var i = 0; i < editors.length; i++) {
+            if (editors[i].file === f) {
+                activateTab(i);
+                return;
+            }
+        }
+        service.get('/services/readfile', {path: f, action: 'read'}, function(err, r) {
+            if (err) {
+                console.log('response: ', err);
+                console.log(err.data.messages["0"].text);
+            } else {
+                console.log('response: ', r);
+                if (r.data.info === "error") {
+					alert(r.data.result)
+                } else if (r.data.info === "file") {					
+                    var language = "ini"; // .conf, .meta
+                    if (/.js$/.test(f)) {
+                        language = "javascript";
+                    } else if (/.xml$/.test(f)) {
+                        language = "xml";
+                    } else if (/.html$/.test(f)) {
+                        language = "html";
+                    } else if (/.css$/.test(f)) {
+                        language = "css";
+                    } else if (/.py$/.test(f)) {
+                        language = "python";
+                    } else if (/.md$/.test(f)) {
+                        language = "markdown";
+                    }
+                    hideAllTabs();
+                    var ecfg = {};
+                    ecfg.container = $("<div></div>").appendTo($container);
+                    ecfg.file = f;
+                    ecfg.tab = $("<div class='ce_tab ce_active'>" + dodgy_basename(f) + "</div>").attr("title", f).appendTo($tabs);
+                    ecfg.last_opened = Date.now();
+                    ecfg.editor = monaco.editor.create(ecfg.container[0], {
+                        value: r.data.result,
+                        language: language,
+                        theme: "vs-dark",
+                    });
+                    ecfg.editor.addCommand(monaco.KeyCode.KEY_S, function() {
+                        //alert('s pressed!');
+                    });
+                    editors.push(ecfg);             
+                } else if (r.data.info === "dir") {
+					inFolder = f;
                     r.data.result.sort();
                     $dirlist.empty();
-                    $("<li><i class='icon-folder'></i> ..</li>").attr("f", f + "/..").appendTo($dirlist)
+                    $("<li class='ce_leftnavfolder'></li>").text(dodgy_basename(f) + '/').attr("f", f).attr("title", f).prepend("<i class='icon-folder'></i> ").append("<i title='Create new folder' class='ce_add_folder ce_right_icon ce_right_two icon-folder'></i><i title='Create new file' class='ce_add_file ce_right_icon icon-report'></i>").appendTo($dirlist);
+                    console.log("'" + f + "'");if (f !== ".") {
+                        $("<li class='ce_leftnav'><i class='icon-folder'></i> ..</li>").attr("f", f.replace(/\/[^\/]+$/,'')).appendTo($dirlist)
+                    }
                     for (var i = 0; i < r.data.result.length; i++) {
                         var icon = "folder";
                         if (r.data.result[i].substr(0,1) === "F") {
                             icon = "report";
                         }
-                        $("<li></li>").text(r.data.result[i].substr(1)).attr("f", f + "/" + r.data.result[i].substr(1)).prepend("<i class='icon-" + icon + "'></i> ").appendTo($dirlist);
+                        $("<li class='ce_leftnav ce_leftnav_editable'></li>").text(r.data.result[i].substr(1)).attr("f", f + "/" + r.data.result[i].substr(1)).prepend("<i class='icon-" + icon + "'></i> ").appendTo($dirlist);
                     }
                 }
                 
             }
         });
     }
-    
+    // TODO replace this with something that comes from the os
+	function dodgy_basename(f) {
+		return f.replace(/.*\//,'')
+	}
     function write(f, c) {
         service.post('/services/writefile', {file: f, contents: c}, function(err, r) {
             if (err) {
--- bin/config-editor.py
+++ bin/config-editor.py
@@ -1,25 +1,74 @@
-import splunk, sys, os, logging, time, json
+import splunk, sys, os, logging, time, json, re, shutil
 
 class readfile(splunk.rest.BaseRestHandler):
 	def handle_GET(self):
 		sessionKey = self.sessionKey
+		textchars = bytearray({7,8,9,10,12,13,27} | set(range(0x20, 0x100)) - {0x7f})
+		is_binary_string = lambda bytes: bool(bytes.translate(None, textchars))
 		try:
-			file_str = self.request['query']['file']
+			result = ""
+			info = ""
+			action = self.request['query']['action']
+			file_str = self.request['query']['path']
 			base_path = [os.path.dirname( __file__ ), '..', '..']
+			base_path_abs = os.path.abspath(os.path.join(*base_path))
 			bits = base_path + file_str.split("/")
 			file_path = os.path.join(*bits)
-			if os.path.isdir(file_path):
-				contents = []
-				for f in os.listdir(file_path):
-					if os.path.isdir(os.path.join(file_path,f)):
-						contents.append("D" + f)
-					else:
-						contents.append("F" + f)
+			file_path_abs = os.path.abspath(file_path)
+			if (len(str(file_path_abs)) < len(str(base_path_abs))):
+				info = "error" 
+				result = "Unable to recurse out of splunk directory"
 			else:
-				with open(file_path, 'r') as fh:
-					contents = fh.read()
+				if action == 'read':
+					if os.path.isdir(file_path):
+						result = []
+						info = "dir"
+						for f in os.listdir(file_path):
+							if os.path.isdir(os.path.join(file_path,f)):
+								# for sorting
+								result.append("D" + f)
+							else:
+								result.append("F" + f)								
+					else:
+						info = "file"
+						with open(file_path, 'r') as fh:
+							result = fh.read()
+						if is_binary_string(result):
+							result = "unable to open binary file"
+							info = "error"
+				elif action == 'delete':
+					if os.path.isdir(file_path):
+						shutil.rmtree(file_path)
+					else:
+						os.remove(file_path)
+					info = "success"
+				else:
+					new_name = self.request['query']['newname']
+					if re.search(r'[^A-Za-z0-9_\- \.]', new_name):
+						result = "New name contains invalid characters"
+						info = "error"
+					elif action == 'rename':						
+						new_path = os.path.join(os.path.dirname(file_path), new_name)
+						if os.path.exists(new_path):
+							result = "That already exists"
+							info = "error"
+						else:
+							os.rename(file_path, new_path)
+							info = "success"
+					else:
+						new_path = os.path.join(file_path, new_name)
+						if os.path.exists(new_path):
+							result = "That already exists"
+							info = "error"
+						elif action == 'newfolder':
+							os.makedirs(new_path)
+							info = "success"
+						elif action == 'newfile':
+							open(new_path, 'w').close()
+							info = "success"
+							
 			self.response.setHeader('content-type', 'application/json')
-			self.response.write(json.dumps({'result': contents}, ensure_ascii=False))
+			self.response.write(json.dumps({'result': result, 'info': info}, ensure_ascii=False))
 			
 		except Exception as ex:
 			template = "An exception of type {0} occurred. Arguments:\n{1!r}"
